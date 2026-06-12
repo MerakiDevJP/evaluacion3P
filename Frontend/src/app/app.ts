101da9a241b5228c67f381cc5181efa5f8ee74a5
nad <pan.tostado.ascii@gmail.com>
2026-06-11 19:31:25 -0500

    feat: implement visitor registration form and entries list components in main application layout

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormUsers } from './components/form-users/form-users';
+import { Entries } from './components/entries/entries';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormUsers, Entries],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
